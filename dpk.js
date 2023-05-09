731a6be35119a56e4fe56e12dc4a32dd37794e56
Nikolay Konovalov <konovalov.nk@gmail.com>
2023-05-09 18:10:42 +0400

    feat(refactor): simplify the main method
    Since we've added multiple edge cases and identified
    that `partitionKey` belongs to a set of scenarios,
    I've decided to extract it into `getPossiblePartitionKey` method.
    
    The `MAX_PARTITION_KEY_LENGTH` logic belongs to
    the new method, since the only case where that might happen
    is when provided `partitionKey` is a string or object
    exceeding this length

--- dpk.js
+++ dpk.js
@@ -9,29 +9,28 @@ exports.hashDigest = (data, hashAlgorithm = DEFAULT_HASH_ALGORITHM) => {
   return crypto.createHash(hashAlgorithm).update(data).digest("hex");
 }
 
+const getPossiblePartitionKey = (event) => {
+  const MAX_PARTITION_KEY_LENGTH = 256;
+  const partitionKey = event?.partitionKey;
+  let candidateKey;
+  if (!partitionKey) { return null; }
+
+  candidateKey = typeof partitionKey !== "string" ? JSON.stringify(partitionKey) : partitionKey;
+
+  if (candidateKey.length > MAX_PARTITION_KEY_LENGTH) {
+    return this.hashDigest(candidateKey);
+  }
+
+  return candidateKey;
+}
+
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+  if (!event) { return TRIVIAL_PARTITION_KEY; }
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (typeof event === "string") {
+    return this.hashDigest(JSON.stringify(event));
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+  return getPossiblePartitionKey(event) || this.hashDigest(event);
 };
